e1ce519394ba31b22c0d371b79b403fd8547e7e6
Michał Bar <logout400@gmail.com>
2021-05-15 20:30:47 +0200

    feat: add icon width constant

--- Shared/Constants.swift
+++ Shared/Constants.swift
@@ -4,5 +4,6 @@ struct Constants {
     struct UI {
         static let cardHeight: CGFloat = 100
         static let cardBorderRadius: CGFloat = 16
+        static let weatherIconMaxWidth: CGFloat = 48
     }
 }
